5513cf7ef2051406be7c54a1ef8f13c688355b30
quiaingmuarua <2424619166@qq.com>
2025-09-09 14:41:56 +0800

    Removed obsolete CMake configurations and build artifacts.

---
+++ CTestTestfile.cmake
@@ -0,0 +1,16 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/d/code_project/geekrun/native_example/open_source/kivm
+# Build directory: /mnt/d/code_project/geekrun/native_example/open_source/kivm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(stack-and-locals "test-stack-and-locals")
+set_tests_properties(stack-and-locals PROPERTIES  _BACKTRACE_TRIPLES "/mnt/d/code_project/geekrun/native_example/open_source/kivm/CMakeLists.txt;346;add_test;/mnt/d/code_project/geekrun/native_example/open_source/kivm/CMakeLists.txt;349;add_test_target;/mnt/d/code_project/geekrun/native_example/open_source/kivm/CMakeLists.txt;0;")
+add_test(encode-decode-offset "test-encode-decode-offset")
+set_tests_properties(encode-decode-offset PROPERTIES  _BACKTRACE_TRIPLES "/mnt/d/code_project/geekrun/native_example/open_source/kivm/CMakeLists.txt;346;add_test;/mnt/d/code_project/geekrun/native_example/open_source/kivm/CMakeLists.txt;350;add_test_target;/mnt/d/code_project/geekrun/native_example/open_source/kivm/CMakeLists.txt;0;")
+add_test(oop-size "test-oop-size")
+set_tests_properties(oop-size PROPERTIES  _BACKTRACE_TRIPLES "/mnt/d/code_project/geekrun/native_example/open_source/kivm/CMakeLists.txt;346;add_test;/mnt/d/code_project/geekrun/native_example/open_source/kivm/CMakeLists.txt;351;add_test_target;/mnt/d/code_project/geekrun/native_example/open_source/kivm/CMakeLists.txt;0;")
+add_test(args-parser "test-args-parser")
+set_tests_properties(args-parser PROPERTIES  _BACKTRACE_TRIPLES "/mnt/d/code_project/geekrun/native_example/open_source/kivm/CMakeLists.txt;346;add_test;/mnt/d/code_project/geekrun/native_example/open_source/kivm/CMakeLists.txt;352;add_test_target;/mnt/d/code_project/geekrun/native_example/open_source/kivm/CMakeLists.txt;0;")
+add_test(native-image "test-native-image")
+set_tests_properties(native-image PROPERTIES  _BACKTRACE_TRIPLES "/mnt/d/code_project/geekrun/native_example/open_source/kivm/CMakeLists.txt;346;add_test;/mnt/d/code_project/geekrun/native_example/open_source/kivm/CMakeLists.txt;353;add_test_target;/mnt/d/code_project/geekrun/native_example/open_source/kivm/CMakeLists.txt;0;")
